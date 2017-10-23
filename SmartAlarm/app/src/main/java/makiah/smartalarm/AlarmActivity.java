7e41d502f1c7e7e10dffd216d97cdcbd808bd8fe
Makiah Bennett <makiahtbennett@gmail.com>
2017-10-23 15:36:11 -0400

    Add Android project structure

---
+++ SmartAlarm/app/src/main/java/makiah/smartalarm/AlarmActivity.java
@@ -0,0 +1,13 @@
+package makiah.smartalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
