ac23398167aa4899088ab1c221ed3c6178d9ffe7
Makiah Bennett <makiahtbennett@gmail.com>
2018-01-08 13:32:28 -0500

    Convert to CV submodules and add C++ build support

---
+++ app/src/main/cpp/native-opencv.cpp
@@ -0,0 +1,31 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+using namespace std;
+using namespace cv;
+
+extern "C"
+{
+
+// More of an example of how Mat modification works in opencv c++
+void JNICALL Java_visionanalysis_CryptoboxDetector_salt(JNIEnv *env, jobject instance,
+                                                                           jlong matAddrGray,
+                                                                           jint nbrElem) {
+    Mat &mGr = *(Mat *) matAddrGray;
+    for (int k = 0; k < nbrElem; k++) {
+        int i = rand() % mGr.cols;
+        int j = rand() % mGr.rows;
+        mGr.at<uchar>(j, i) = 255;
+    }
+}
+
+// Gets the mat between a threshold of two mats instead of two scalars.
+void JNICALL Java_hankextensions_vision_opencv_OpenCVJNIHooks_inRangeBetweenMatsNative(JNIEnv *env, jobject instance, jlong toFilterAddr, jlong lowerAddr, jlong upperAddr, jlong destAddr)
+{
+    inRange(*(Mat *) toFilterAddr, *(Mat *) lowerAddr, *(Mat *) upperAddr, *(Mat *) destAddr);
+}
+
+}
